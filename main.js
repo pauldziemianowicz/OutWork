2a13d1da38ebcc71a13f9de898b541301827c091
Paul Dziemianowicz <Paul@Pauls-MacBook-Pro.local>
2016-05-06 05:30:13 -0600

    updated home.html, main.js, style.css -- significant changes

--- main.js
+++ main.js
@@ -15,15 +15,20 @@ $(function () {
 
   var dashCatSelected = "sleep";    // = selected dashboard category (default)
   var subMenuSelected = "overview"; // = selected dashboard submenu item (default)
+  var currentDashboardDisplay = "";
 
   // localStorage object storing up-to-date time/date data
   var localDateInfo = {
     currentTime: "",
     currentTimeOld: "",
+    currentYear: "",
+    currentMonth: "",
+    currentMonthText: "",
     currentDoM: "",
     currentDoMOld: "",
     currentDoW: "",
     currentDoWOld: "",
+    currentDoWText: "",
     currentHour: "",
     currentHourOld: "",
     currentMinute: "",
@@ -32,7 +37,14 @@ $(function () {
     currentSecondOld: "",
   };
 
-  var sleepData = {};       // for storing and updating sleep data in local storage
+
+  var sleepData = {
+    today: {
+      sleepTime: "",
+      wakeTime: "",
+    }
+  };       // for storing and updating sleep data in local storage
+
   var nutritionData = {};   // for storing and updating nutrition data in local storage
   var exerciseData = {};    // for storing and updating exercise data in local storage
 
@@ -56,6 +68,8 @@ $(function () {
       localStorage2 = localStorage.userAccountData2;
       localDateInfo.accountDayCounter = 0;
       localDateInfo.testCounter = 0;
+      sleepNeedsUpdate = true;
+      localStorage.localDateInfo = JSON.stringify(localDateInfo);
       console.log(localStorage2);
     }
   };
@@ -98,8 +112,12 @@ $(function () {
 // 2 - Dashboard and SubMenu Selection Display Mechanism
 // -------------------------------------------------
 
+$('h1#welcome1').html("Welcome,  " + JSON.parse(localStorage.userAccountData2).userFirstName);
+
+
 $('nav.dashboard-bottom-nav').css("visibility", "hidden");
 
+// ---------- Display Selected Category Tab
 function dashCatSelectDisplay() {
   if(dashCatSelected === "sleep") {
     $('#sleep-category').css("background-color", "rgba(62, 21, 195, 0.5)");
@@ -118,6 +136,7 @@ function dashCatSelectDisplay() {
   }
 };
 
+// ---------- Display Selected Sub Menu Tab
 function subMenuSelectDislay() {
   if(subMenuSelected === "overview") {
     $('.sub-overview').css("background-color", "rgb(255, 255, 255)");
@@ -146,15 +165,88 @@ function subMenuSelectDislay() {
   }
 };
 
+  // ---------- Dashboard Navigation & Page Display
+  function displayDashboardSection() {
+    if(dashCatSelected === "sleep") {
+      $('.welcome').css('display', 'none');
+
+      $('.dashboard-nutrition').css('display', 'none');
+      $('.dashboard-exercise').css('display', 'none');
+      $('.dashboard-sleep').css('display', 'flex');
+
+      if(subMenuSelected === "overview") {
+        $('.sleep-update').css('display', 'none');
+        $('.sleep-goals').css('display', 'none');
+        $('.sleep-log').css('display', 'none');
+        $('.sleep-overview').css('display', 'flex');
+      }
+      else if(subMenuSelected === "update") {
+        $('.sleep-goals').css('display', 'none');
+        $('.sleep-log').css('display', 'none');
+        $('.sleep-overview').css('display', 'none');
+        $('.sleep-update').css('display', 'flex');
+      }
+      else if(subMenuSelected === "goals") {
+        $('.sleep-log').css('display', 'none');
+        $('.sleep-overview').css('display', 'none');
+        $('.sleep-update').css('display', 'none');
+        $('.sleep-goals').css('display', 'flex');
+      }
+      else if(subMenuSelected === "log") {
+        $('.sleep-overview').css('display', 'none');
+        $('.sleep-update').css('display', 'none');
+        $('.sleep-goals').css('display', 'none');
+        $('.sleep-log').css('display', 'flex');
+      }
+    }
+    else if(dashCatSelected === "nutrition") {
+      $('h1.welcome').css('display', 'none');
+
+      $('.dashboard-sleep').css('display', 'none');
+      $('.dashboard-exercise').css('display', 'none');
+      $('.dashboard-nutrition').css('display', 'flex');
+    }
+    else if(dashCatSelected === "exercise") {
+      $('h1.welcome').css('display', 'none');
+
+      $('.dashboard-sleep').css('display', 'none');
+      $('.dashboard-nutrition').css('display', 'none');
+      $('.dashboard-exercise').css('display', 'flex');
+    }
+  };
+
+function setUpdateDisplay() {
+  if(sleepNeedsUpdate === false) {
+    $('#sleepUpdate').css('display', 'none');
+    $('#sleepUpdated').css('display', 'flex');
+  } else if(sleepNeedsUpdate === true) {
+    $('#sleepUpdated').css('display', 'none');
+    $('#sleepUpdate').css('display', 'flex');
+  }
+};
+
+
 $('#sleep-category').on('click', function(){
   dashCatSelected = "sleep";
-  subMenuSelected = "overview";
+  if(sleepNeedsUpdate === false) {
+    subMenuSelected = "overview";
+  } else if (sleepNeedsUpdate === true) {
+    subMenuSelected = "update";
+  }
+  setUpdateDisplay();
   console.log(dashCatSelected);
   console.log(subMenuSelected);
   $('nav.dashboard-bottom-nav').css("visibility", "visible");
   dashCatSelectDisplay();
   subMenuSelectDislay();
+  displayDashboardSection();
+  setTodayDateText();
+  displayDate();
 });
+
+function displayDate() {
+  $('.today-date').html(localDateInfo.currentDoWText + ", " + localDateInfo.currentMonthText + " " + localDateInfo.currentDoM + " - " + localDateInfo.currentYear);
+};
 
 $('#nutrition-category').on('click', function(){
   dashCatSelected = "nutrition";
@@ -164,6 +256,9 @@ $('#nutrition-category').on('click', function(){
   $('nav.dashboard-bottom-nav').css("visibility", "visible");
   dashCatSelectDisplay();
   subMenuSelectDislay();
+  displayDashboardSection()
+  setTodayDateText();
+  displayDate();
 });
 
 $('#exercise-category').on('click', function(){
@@ -174,26 +269,34 @@ $('#exercise-category').on('click', function(){
   $('nav.dashboard-bottom-nav').css("visibility", "visible");
   dashCatSelectDisplay();
   subMenuSelectDislay();
+  displayDashboardSection();
+  setTodayDateText();
+  displayDate();
 });
 
 $('.sub-overview').on('click', function() {
   subMenuSelected = "overview";
   subMenuSelectDislay();
+  displayDashboardSection();
 });
 
 $('.sub-update').on('click', function() {
   subMenuSelected = "update";
   subMenuSelectDislay();
+  setUpdateDisplay();
+  displayDashboardSection();
 });
 
 $('.sub-goals').on('click', function() {
   subMenuSelected = "goals";
   subMenuSelectDislay();
+  displayDashboardSection();
 });
 
 $('.sub-log').on('click', function() {
   subMenuSelected = "log";
   subMenuSelectDislay();
+  displayDashboardSection();
 });
 
 // DATE & TIME REFERENCE
@@ -201,7 +304,8 @@ $('.sub-log').on('click', function() {
 // ---------------------
 
 var lsDateObject = JSON.parse(localStorage.localDateInfo);
-var needsUpdate = false;
+
+var sleepNeedsUpdate = false;
 
 console.log("recent local storage");
 console.log(lsDateObject);
@@ -224,32 +328,34 @@ function validateNewMinute() {
   // }
 
   function countMinuteMS() {
-    console.log(needsUpdate);
+    console.log(sleepNeedsUpdate);
     console.log(localDateInfo.currentMinuteOld);
     console.log(localDateInfo.currentMinute);
     console.log(localDateInfo.currentSecond);
-    if(localDateInfo.currentMinute !== localDateInfo.currentMinuteOld) {
-      needsUpdate = true;
-    };
-    if(localDateInfo.currentSecond > 55) {
-      needsUpdate = false;
-    }
-    console.log(needsUpdate);
+    // if(localDateInfo.currentMinute !== localDateInfo.currentMinuteOld) {
+    //   sleepNeedsUpdate = true;
+    // };
+    // if(localDateInfo.currentSecond > 55) {
+    //   sleepNeedsUpdate = false;
+    // }
+    // console.log(sleepNeedsUpdate);
     var now = new Date();
     var nowMS = now.getTime();
-    console.log(nowMS);
+    // console.log(nowMS);
     setTimeout(countMinuteMS, 500)
   }
 }
+
+resumeTestCounter();
+
+storeDateInfo();
 
 pullOldFromLSOnInit();
 
 validateNewMinute();
 
-resumeTestCounter();
-
-storeDateInfo();
 setInterval(storeDateInfo, 1000);
+
 
 // A - Resume Counter
 // ------------------
@@ -281,15 +387,6 @@ function resumeTestCounter() {
 // If currentMinuteOld is an empty string "" (as set upon page refresh with variable
 // assignment, set it to localStorage value of currentMinute which has just been defined
 // -- if currentMinute is empty string, then we know it's the user's first time on the site.
-
-// function updateLSDateInfo() {
-//   if(localDateInfo.currentMinuteOld === "") {
-//     localDateInfo.currentMinuteOld = localDateInfo.currentMinute;
-//   }
-//   localStorage.localDateInfo = JSON.stringify(localDateInfo);
-//   console.log(localStorage.localDateInfo);
-//   console.log(JSON.parse(localStorage.localDateInfo));
-// };
 
 
 // B -
@@ -331,16 +428,18 @@ function updateLSDateInfo() {
 function storeDateInfo() {
   var currentTimeL = new Date();
   localDateInfo.currentTime = currentTimeL;
+  localDateInfo.currentYear = currentTimeL.getFullYear();
+  localDateInfo.currentMonth = currentTimeL.getMonth();
   localDateInfo.currentDoM = currentTimeL.getDate();
   localDateInfo.currentDoW = currentTimeL.getDay();
   localDateInfo.currentHour = currentTimeL.getHours();
   localDateInfo.currentMinute = currentTimeL.getMinutes();
   localDateInfo.currentSecond = currentTimeL.getSeconds();
   // updates current___ properties to LS from localDateInfo
+  validateNewFakeDay();
   updateLSDateInfo();
   // console.log("currentTime is more recent than currentTimeOld");
   setTimeout(saveCurrentToCurrentOld, 500);
-  validateNewFakeDay();
 };
 
 // before this function runs (between storeDataInfo call and 2500ms after
@@ -369,16 +468,141 @@ function validateNewFakeDay() {
   // }
   if (newMinute !== oldMinute) {
     localDateInfo.testCounter += 1;
+    sleepNeedsUpdate = true;
     console.log(localDateInfo.testCounter);
     console.log(localDateInfo.testCounter);
     console.log(localDateInfo.testCounter);
     console.log(localDateInfo.testCounter);
     console.log(localDateInfo.testCounter);
+    // sleepData[]
     }
   };
 
+// Set Text Display for Current Date
+
+function setTodayDateText() {
+  if(localDateInfo.currentDoW) {
+    if(localDateInfo.currentDoW === 0) {
+      localDateInfo.currentDoWText = "Sunday";
+    }
+    else if(localDateInfo.currentDoW === 1) {
+      localDateInfo.currentDoWText = "Monday";
+    }
+    else if(localDateInfo.currentDoW === 2) {
+      localDateInfo.currentDoWText = "Tuesday";
+    }
+    else if(localDateInfo.currentDoW === 3) {
+      localDateInfo.currentDoWText = "Wednesday";
+    }
+    else if(localDateInfo.currentDoW === 4) {
+      localDateInfo.currentDoWText = "Thursday";
+    }
+    else if(localDateInfo.currentDoW === 5) {
+      localDateInfo.currentDoWText = "Friday";
+    }
+    else if(localDateInfo.currentDoW === 6) {
+      localDateInfo.currentDoWText = "Saturday";
+    }
+  };
+  if(localDateInfo.currentMonth) {
+    if(localDateInfo.currentMonth === 0) {
+      localDateInfo.currentMonthText = "January";
+    }
+    else if(localDateInfo.currentMonth === 1) {
+      localDateInfo.currentMonthText = "February";
+    }
+    else if(localDateInfo.currentMonth === 2) {
+      localDateInfo.currentMonthText = "March";
+    }
+    else if(localDateInfo.currentMonth === 3) {
+      localDateInfo.currentMonthText = "April";
+    }
+    else if(localDateInfo.currentMonth === 4) {
+      localDateInfo.currentMonthText = "May";
+    }
+    else if(localDateInfo.currentMonth === 5) {
+      localDateInfo.currentMonthText = "June";
+    }
+    else if(localDateInfo.currentMonth === 6) {
+      localDateInfo.currentMonthText = "July";
+    }
+    else if(localDateInfo.currentMonth === 7) {
+      localDateInfo.currentMonthText = "August";
+    }
+    else if(localDateInfo.currentMonth === 8) {
+      localDateInfo.currentMonthText = "September";
+    }
+    else if(localDateInfo.currentMonth === 9) {
+      localDateInfo.currentMonthText = "October";
+    }
+    else if(localDateInfo.currentMonth === 10) {
+      localDateInfo.currentMonthText = "November";
+    }
+    else if(localDateInfo.currentMonth === 11) {
+      localDateInfo.currentMonthText = "December";
+    }
+  }
+  console.log(localDateInfo.currentMonthText);
+  console.log(localDateInfo.currentDoWText);
+  console.log(localDateInfo.currentYear);
+};
+
+
 // Storing and Interacting with Sleep data
+
+$('form').on("submit", function(e){
+  e.preventDefault();
+  sleepNeedsUpdate = false;
+  setUpdateDisplay(); 
+
+  var newSleepTime = $('input[name=yesterdaySleepTime]')[0].value;
+  var newWakeTime = $('input[name=todayWakeTime]')[0].value;
+  var form = $('#sleepUpdateForm')[0];
+
+  var newSleepData = new NewSleepData(newSleepTime, newWakeTime, localDateInfo.testCounter);
+  console.log(newSleepData);
+  eval("sleepData.d" + localDateInfo.testCounter + "=newSleepData");
+  eval("localStorage.sleepData.d" + localDateInfo.testCounter + "=JSON.stringify(newSleepData)");
+  localStorage.setItem(sleepData, sleepData);
+
+
+  sleepData.today.sleepTime = newSleepTime;
+  sleepData.today.wakeTime = newWakeTime;
+
+  form.reset();
+
+  console.log(sleepData);
+  console.log(localStorage.sleepData);
+})
+
+
+function NewSleepData(sleepTime, wakeTime, dayNumber) {
+  this.sleepTime = sleepTime;
+  this.wakeTime = wakeTime;
+  this.dayNumber = dayNumber;
+}
+
+// var newSleepData = new NewSleepData(newSleepTime, newWakeTime, localDateInfo.testCounter);
+// eval("sleepData.d" + localDateInfo.testCounter + "=newSleepData");
+
+//
+// var testObject = {};
+// eval("var d" + 1 + "='itworked';");
+// console.log(d1);
+// var evalTest = new NewSleepData("one", "two", localDateInfo.testCounter);
+// console.log(evalTest);
+// eval("testObject.d" + localDateInfo.testCounter + "=evalTest");
+// console.log(testObject)
+
+
 // Storing and Interacting with Nutrition data
 // Storing and Interacting with Exercise data
+
+
+
+
+
+
+
 
 });
